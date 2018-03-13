9ee5219fd5fca6f37de2e6ef97481e9daec71dcb
DeanJoseph93 <deanjoseph3@hotmail.com>
2018-03-13 15:07:09 +0000

    further updates to index file

--- build/js/script.js
+++ build/js/script.js
@@ -2,13 +2,23 @@
 
  let
 
+     /**
+      * @name:   navToggle()
+      * @desc:   Hamburger changes to x, once clicked
+      */
 
+     navToggle = () => {
+        $("#nav-icon1").click(function () {
+            $(this).toggleClass('open');
+
+        });
+    }; //navToggle
 
 
 
 
   init = () => {
-
+      navToggle();
   }; //init
 
   window.addEventListener("load", init);
