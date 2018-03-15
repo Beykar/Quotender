b3e485b77564602098f1cb202ac2f7f15bab1685
DeanJoseph93 <deanjoseph3@hotmail.com>
2018-03-15 13:09:44 +0000

    added the count plugin in the 'reasons' section

--- build/js/script.js
+++ build/js/script.js
@@ -1,6 +1,7 @@
 ( () => {
 
  let
+     animated = false,
 
 
      /**
@@ -15,12 +16,43 @@
         });
     }, //navToggle
 
+    countFunction = () => {
+
+         $('#innerReasons').waypoint(function () {
+
+             if ( animated == false) {
+
+                $('.count').each(function () {
+                    $(this).prop('Counter', 0).animate({
+                        Counter: $(this).text()
+                    }, {
+                        duration: 5000,
+                        easing: 'swing',
+                        step: function (now) {
+                            $(this).text(Math.ceil(now));
+                        }
+                    });
+                });
+                 animated = true;
+             }//end if animated
+         }, {
+             offset: '100%'
+         });
 
 
+
+        return animated;
+
+
+    }, //countFunction
 
 
   init = () => {
       navToggle();
+
+      countFunction();
+
+
 
 
 
