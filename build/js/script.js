25a035b5c6768eaed1eaf547d2df6025268a56ff
Beykar <hamidkhaldi@aol.com>
2018-03-16 19:08:15 +0000

    css changes to Category section in smaller devices

--- build/js/script.js
+++ build/js/script.js
@@ -31,12 +31,12 @@
 
                 $('.count').each(function () {
                     $(this).prop('Counter', 0).animate({
-                        Counter: $(this).text().toLocaleString()
+                        Counter: $(this).text()
                     }, {
                         duration: 5000,
                         easing: 'swing',
                         step: function (now) {
-                            $(this).text(Math.ceil(now)).toLocaleString();
+                            $(this).text(Math.ceil(now)).toLocaleString('en-US');
                         }
                     });
                 });
