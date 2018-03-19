a9229474a8e9205b190d5da72abca5d37d672879
Anabel Sposato <sposatoanabel@gmail.com>
2018-03-19 12:21:00 +0000

    testimonial slider working with slick slider plugin

--- build/js/script.js
+++ build/js/script.js
@@ -54,11 +54,47 @@
     }, //countFunction
 
 
+     playTestimonialSlider =()=>{
+         $(".slider").slick({
+
+             // normal options...
+             infinite: false,
+             autoplaySpeed:500,
+             autoplay:true,
+
+             // the magic
+             responsive: [{
+
+                 breakpoint: 1024,
+                 settings: {
+                     slidesToShow: 1,
+                     infinite: true
+                 }
+
+             }, {
+
+                 breakpoint: 600,
+                 settings: {
+                     slidesToShow: 1,
+                     dots: true
+                 }
+
+             }, {
+
+                 breakpoint: 300,
+                 settings: "unslick" // destroys slick
+
+             }]
+         });
+
+     }, //playTestimonialSlider
+
   init = () => {
       navToggle();
 
       countFunction();
 
+      playTestimonialSlider();
 
 
 
